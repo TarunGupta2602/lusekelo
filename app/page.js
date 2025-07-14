8d6b0ef45d6704e50535c2f7b567a01dd0bf6502
Tarun Gupta <itstarungarg07@gmail.com>
2025-07-14 12:44:05 +0530

    ho ja yrr

--- app/page.js
+++ app/page.js
@@ -1,18 +1,18 @@
 import ShopContent from "./ShopContent";
 
-export const revalidate = 3600; // ISR every 1 hour
+export const revalidate = 3600; // ISR every hour
 
 export default async function HomePage() {
   let stores = [];
 
   try {
-    const baseUrl =
-      process.env.VERCEL_URL // Automatically set in Vercel
-        
+    const baseUrl = process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}`
+      : process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
+
     const res = await fetch(`${baseUrl}/api/stores`, {
       next: { revalidate: 3600 },
       headers: {
-        // Ensure it's treated as a server-side fetch
         "Content-Type": "application/json",
       },
     });
